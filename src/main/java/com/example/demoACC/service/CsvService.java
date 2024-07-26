10d8ad83e6dc72a41e7b80056dc308356cdf158d
Aaryen D'souza <dc.aaryen@gmail.com>
2024-07-25 23:12:04 -0400

    corrected CsvService.java file, had unwanted variables

--- src/main/java/com/example/demoACC/service/CsvService.java
+++ src/main/java/com/example/demoACC/service/CsvService.java
@@ -5,7 +5,6 @@ import com.example.demoACC.model.Plan;
 import com.opencsv.CSVReader;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,40 +15,7 @@ public class CsvService {
     private List<Mobile> mobiles = new ArrayList<>();
     private List<Plan> plans = new ArrayList<>();
 
-    @PostConstruct
-    public void init() {
-//        try (CSVReader reader = new CSVReader(new InputStreamReader(getClass().getResourceAsStream("/fido.csv")))) {
-//            String[] line;
-//            boolean isMobile = true;
-//            reader.readNext(); // Skip header
-//            while ((line = reader.readNext()) != null) {
-////                System.out.println(Arrays.toString(line));
-////                System.out.println(line.length);
-//                if (line.length == 1) {
-//                    isMobile = false;
-//                    reader.readNext(); // Skip second header
-//                    reader.readNext(); // Skip second header
-//                    continue;
-//                }
-//                if (isMobile) {
-//                    Mobile mobile = new Mobile();
-//                    mobile.setMobileName(line[0]);
-//                    mobile.setMobilePlanPrice(line[1]);
-//                    mobile.setMobileFullPrice(line[2]);
-//                    mobile.setMobileImageUrl(line[3]);
-//                    mobiles.add(mobile);
-//                } else {
-//                    Plan plan = new Plan();
-//                    plan.setPlanName(line[0]);
-//                    plan.setPlanCharge(line[1]);
-//                    plan.setPlanBenefits(line[2]);
-//                    plans.add(plan);
-//                }
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-    }
+    
 
     public List<Mobile> getMobiles() {
 //        System.out.println("MobilePlansRanNow");
@@ -79,11 +45,9 @@ public class CsvService {
     }
 
     public List<Plan> getPlans() {
-//        System.out.println("SimPlansRanNow");
         try (CSVReader reader = new CSVReader(new InputStreamReader(getClass().getResourceAsStream("/fido.csv")))) {
             String[] line;
             boolean isMobile = false;
-            int c = 0;
             reader.readNext(); // Skip header
             while (!reader.readNext()[0].equals("Plan Name")) {
                 continue;
@@ -92,7 +56,6 @@ public class CsvService {
             while (((line = reader.readNext()) != null)) {
                 System.out.println(Arrays.toString(line));
                 if (!isMobile && (line.length != 1)) {
-                    c = 1;
                     Plan plan = new Plan();
                     plan.setPlanName(line[0]);
                     plan.setPlanCharge(line[1]);
